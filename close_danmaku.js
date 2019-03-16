4d5ba124f54cedf7139fea68980691db3e624fd6
yekingyan <529616@gmail.com>
2019-03-16 14:19:53 +0800

    默认关闭bilibili弹幕

---
+++ close_danmaku.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         默认关闭弹幕
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  默认关闭弹幕
+// @author       Yekingyan
+// @match        https://www.bilibili.com/*
+// @run-at        document-start
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let host = window.location.host
+    let TARGET_AND_MARK = new Map ([
+        // [[hostName, switchSelector], [markSelector, markText]],
+        [['bilibili', '.bilibili-player-video-danmaku-switch .bui-checkbox'], 
+        ['.bilibili-player-video-danmaku-switch .choose_danmaku', '关闭']],
+
+    ])
+
+    let closeDanmaku = () => {
+        for (let [[hostName, switchSelector], [markSelector, markText]] of TARGET_AND_MARK) {
+            if (host.includes(hostName)) {
+                let [switchDom, markDom] = [switchSelector, markSelector].map(x => document.querySelector(x))
+                if (!markDom) {
+                    switchDom.click()
+                } else if (markDom.innerText.includes(markText)) {
+                    switchDom.click()
+                }
+                break
+            }
+        }
+    }
+
+    window.onload = () => {
+        setTimeout(() => {
+            closeDanmaku()
+        }, 2000);
+    }
+    
+    
+})();
